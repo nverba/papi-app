e62c927a3c9f4563b1d1a93d8dee89ca28764b09
nverba <distantstate@gmail.com>
2015-09-01 15:24:36 +0100

    Partially implemented server + template with material design

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ var papiClient = require('papic.js');
 papiClient.route(app);
 
 app.use('/papi', express.static(papiClient.staticPath));
+app.use('/components', express.static('./components'));
+app.use('/material', express.static('./node_modules/material-design-lite/dist'));
 app.use('/', express.static('./public'));
+
 
 var server = app.listen(process.env.PORT || 3000, function(){
     var addr = server.address();
